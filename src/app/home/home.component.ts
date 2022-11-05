3aa8fd78f8385dc67e835e20fc9f3ef2c1a6caed
Amine Bouzahar <mohamed.amine.bouzahar@gmail.com>
2022-11-05 04:35:36 +0100

    update dependencies

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,9 @@ export class HomeComponent implements OnInit {
   party: Party | undefined;
   playersNumber: any = { title: "4 players", value: 4 };
   selectModel = [
-    { title: "1 player", value: 1, },
-    { title: "2 players", value: 2, },
-    { title: "3 players", value: 3, },
     { title: "4 players", value: 4, },
     { title: "5 players", value: 5, },
+    { title: "6 players", value: 6, },
   ]
 
   ngOnInit(): void {
