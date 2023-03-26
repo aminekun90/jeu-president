1715cf2410a4fa44a1490dd2e06ca74f526c4690
Amine Bouzahar <mohamed.amine.bouzahar@gmail.com>
2023-03-26 08:26:17 +0200

    Update 0.0.2 alpha, add scrolling between cards using mouse wheel while hovering on a hand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Card } from "./model/card";
 @Component({
   selector: 'app-root',
@@ -7,18 +7,21 @@ import { Card } from "./model/card";
 })
 export class AppComponent {
   title = 'daifuqo';
-  gameMenuOpened:boolean =false;
+  gameMenuOpened: boolean = false;
   deck: any[] = [];
   hands: Array<Array<Card>> = [];
-  gameState:string = 'stopped';
+  hoveredHand: any = null;
+  gameState: string = 'stopped';
   playersNumber: any = { title: "3 players", value: 3 };
+  timer:any;
+  clearedTimer:boolean=true;
   selectModel = [
     { title: "3 players", value: 3, },
     { title: "4 players", value: 4, },
     { title: "5 players", value: 5, },
     { title: "6 players", value: 6, },
   ]
-  openGameMenu(){
+  openGameMenu() {
     this.gameMenuOpened = true;
   }
   initializeDeck() {
@@ -35,6 +38,48 @@ export class AppComponent {
       this.hands[i] = [];
   }
 
+  @HostListener('mousewheel', ['$event']) // for window scroll events
+  onScroll(event: any) {
+    if(this.hoveredHand && this.clearedTimer){
+      this.clearedTimer=false;
+      event.preventDefault();
+      this.timer = setTimeout(()=>{
+        console.log(this.hoveredHand.value);
+
+        if(event.wheelDelta>100)
+        this.moveArrayElementsForward(this.hoveredHand.value)
+        if(event.wheelDelta<100)
+        this.moveArrayElementsBackward(this.hoveredHand.value)
+        clearTimeout(this.timer);
+        this.clearedTimer=true;
+      },300);
+    }
+  }
+
+  moveArrayElementsForward(arr:Card[]) {
+    if (arr.length <= 1) return arr;
+
+    const lastElement = arr.pop() as Card;
+    arr.unshift(lastElement);
+
+    return arr;
+  }
+
+  moveArrayElementsBackward(arr:Card[]) {
+    if (arr.length <= 1) return arr;
+
+    const firstElement = arr.shift() as Card;
+    arr.push(firstElement);
+
+    return arr;
+  }
+
+  mouseEnterDeck(hand: any) {
+    this.hoveredHand = hand;
+  }
+  mouseLeaveDeck(hand: any) {
+    this.hoveredHand = null;
+  }
   shuffle($deck: Array<Array<any>>) {
     let currentIndex = $deck.length, randomIndex
     // While there remain elements to shuffle.
@@ -54,33 +99,26 @@ export class AppComponent {
   }
 
   giveCards() {
-    let numberofplayers = parseInt(this.playersNumber.value);
-    let devider = (this.deck.length / numberofplayers) - (this.deck.length % numberofplayers);
-    let array: number[][] = []
-    for (let i = 0; i < numberofplayers; i++) {
-      array[i] = [];
-      for (let j = 0; j < devider; j++) {
-        if (!i && !j) {
-          array[i][j] = 0;
-        } else if (i && !j) {
-          array[i][j] = array[i - 1][j] + 1;
-        } else if (!i && j) {
-          array[i][j] = array[i][j - 1] + numberofplayers;
-        } else if (i && j) {
-          array[i][j] = array[i - 1][j] + 1;
-        }
+    const numberOfPlayers = parseInt(this.playersNumber.value);
+    const cardsPerPlayer = Math.floor(this.deck.length / numberOfPlayers);
+
+    this.hands = Array.from({ length: numberOfPlayers }, () => []);
+
+    for (let i = 0; i < cardsPerPlayer; i++) {
+      for (let j = 0; j < numberOfPlayers; j++) {
+        const cardIndex = i * numberOfPlayers + j;
+        this.hands[j].push(this.deck[cardIndex]);
       }
-      array.forEach((x, i) => this.hands[i] = x.map(indexes => this.deck[indexes]));
     }
   }
 
   onStartClick() {
-    console.log("Game started",this.playersNumber);
-    this.gameState ='started';
+    console.log("Game started", this.playersNumber);
+    this.gameState = 'started';
     this.initializeDeck();
     this.deck = this.shuffle([...this.deck]);
     this.giveCards();
     console.log(this.hands);
-    this.gameMenuOpened=false;
+    this.gameMenuOpened = false;
   }
 }
